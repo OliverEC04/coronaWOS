ffdcb10fa3e421e45d735e9e1ee28bd98b8740f7
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-03-13 13:51:55 +0100

    pos

--- cirkel.js
+++ cirkel.js
@@ -3,13 +3,12 @@ class Cirkel
   constructor(status)
   {
     this.status = status;
-    this.farve = (0);
+    this.farve = color(0);
+    this.position = createVector(random(0, window.x), random(0, window.y));
   }
 
   update()
   {
-    this.position = createVector(random(0, window.x), random(0, window.y));
-
     if (this.status == 1)
     {
       this.farve = color(255, 0, 0);
