e73c77c59cd6a72214e6a44d555fdd7ce522698c
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2020-03-15 16:31:24 +0100

    test

--- cirkel.js
+++ cirkel.js
@@ -15,25 +15,41 @@ class Cirkel
       this.farve = color(255, 0, 0);
     }
     
-    this.position.x += random(-2, 2);
-    this.position.y += random(-2, 2);
+    if (this.position.x - this.radius > 0)
+    {
+      this.position.x += random(-2, 2);
+    }
+    if (this.position.x + this.radius < window.x)
+    {
+      this.position.x += random(-2, 2);
+    }
 
-    if (this.position.x < 0)
+    if (this.position.y - this.radius > 0)
     {
-      this.position.x++;
+      this.position.y += random(-2, 2);
     }
-    if (this.position.x > window.x)
+    if (this.position.y + this.radius < window.y)
     {
-      this.position.x--;
-    }
-    if (this.position.y < 0)
+      this.position.y += random(-2, 2);
+
+    /*
+    if (this.position.x - this.radius <= 0)
     {
-      this.position.y++;
+      this.position.x += 2;
     }
-    if (this.position.y > window.y)
+    if (this.position.x + this.radius >= window.x)
     {
-      this.position.y--;
+      this.position.x -= 2;
     }
+    if (this.position.y - this.radius <= 0)
+    {
+      this.position.y += 2;
+    }
+    if (this.position.y + this.radius >= window.y)
+    {
+      this.position.y -= 2;
+    }
+    */
   }
 
   draw()
