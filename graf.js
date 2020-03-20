1249a2d6b89e865099df9cb6538126598996d921
OliverEC04 <oliverec04@gmail.com>
2020-03-20 10:41:16 +0100

    graf

--- graf.js
+++ graf.js
@@ -1,7 +1,3 @@
-class Graf
-
-<canvas id="myChart" width="400" height="400"></canvas>
-<script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
