95e8f8c6980b79934c55337cd5bb546ff48d6785
SimonThule <simonthulek@gmail.com>
2020-03-20 10:36:34 +0100

    nygraftest

--- graf.js
+++ graf.js
@@ -1,5 +1,8 @@
 class Graf
 
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
