c68ef3d09b654e3cf1d58810851b2a6e85def354
OliverEC04 <oliverec04@gmail.com>
2020-03-23 10:35:55 +0100

    r

--- forklaring.js
+++ forklaring.js
@@ -1,7 +1,7 @@
 function forklaring(pos, str)
 {
     statusAntal = 5;
-    fill()
+    fill(0)
     rect(pos.x, pos.y + str.y, str.x / statusAntal, 20);
 }
     // 0 = ikke smittet, 1 = smittet, 2 = helbredt, 3 = fool, 4 = død
