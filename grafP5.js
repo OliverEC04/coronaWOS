0f5a524c03c34483c908249361d34c43af058142
Trojel <wtrojel@gmail.com>
2020-04-03 11:00:37 +0200

    d

--- grafP5.js
+++ grafP5.js
@@ -20,5 +20,10 @@ class Graf
         line(this.pos.x, this.pos.y + this.str.y, this.pos.x + this.str.x, this.pos.y + this.str.y); //x-akse
         line(this.pos.x, this.pos.y + this.str.y, this.pos.x, this.pos.y); //y-akse
         
+    for (i = 0; i < this.yMax; i++)
+        {
+            line(this.xData[i], smittetData[i], this.this.xData[i-1], smittetData[i-1] );
+        }
+        
     }
 }
